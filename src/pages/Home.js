b9df78c41131d68f7c237fdf11c057a8bd6545c5
Maksim Borovik <borovik.maksim@gmail.com>
2020-03-27 17:29:17 +0200

    Navbar, Routing, main page, alert and search components were done

---
+++ src/pages/Home.js
@@ -0,0 +1,32 @@
+import React, {Fragment} from "react";
+import {Search} from "../components/Search";
+import {Card} from "../components/Card";
+
+
+export const Home = () => {
+
+  const cards = new Array(15)
+    .fill('')
+    .map((_, i) => i)
+  console.log(cards)
+  return (
+    <Fragment>
+      <Search />
+
+      <div className="row">
+
+        {cards.map(card => (
+          
+          <div key={card} className="col-sm-4 mb-4">
+            <Card />
+          </div>
+          )
+        )}
+
+
+      </div>
+
+
+    </Fragment>
+  )
+}
